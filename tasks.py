3f277f790d5c374c62e9ed6590f5822da860df4c
Addapu Praharika <praharika18@users.noreply.huggingface.co>
2026-04-01 18:23:15 +0000

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,17 +1,7 @@
 tasks = [
-    {
-        "difficulty": "easy",
-        "job": "cotton harvesting",
-        "required_skill": "cotton"
-    },
-    {
-        "difficulty": "medium",
-        "job": "rice planting",
-        "required_skill": "rice"
-    },
-    {
-        "difficulty": "hard",
-        "job": "pesticide spraying",
-        "required_skill": "spraying"
-    }
+    {"job": "cotton harvesting", "required_skill": "cotton", "difficulty": "easy"},
+    {"job": "pesticide spraying", "required_skill": "spraying", "difficulty": "hard"},
+    {"job": "rice planting", "required_skill": "rice", "difficulty": "medium"},
+    {"job": "tractor ploughing", "required_skill": "tractor", "difficulty": "hard"},
+    {"job": "irrigation management", "required_skill": "water", "difficulty": "medium"}
 ]
