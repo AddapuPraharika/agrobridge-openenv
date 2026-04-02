da13611ccde6a471412d753b2d8e1b0cab0c37fa
Addapu Praharika <praharika18@users.noreply.huggingface.co>
2026-04-02 05:59:03 +0000

    Update models.py

--- models.py
+++ models.py
@@ -1,21 +1,23 @@
 # models.py
 
 class Farmer:
-def **init**(self, name: str, skill: str):
-self.name = name
-self.skill = skill
+    def __init__(self, name: str, skill: str):
+        self.name = name
+        self.skill = skill
+
 
 class AgroBridgeAction:
-def **init**(self, message: str):
-self.message = message
+    def __init__(self, message: str):
+        self.message = message
+
 
 class AgroBridgeObservation:
-def **init**(self, echoed_message: str):
-self.echoed_message = echoed_message
+    def __init__(self, echoed_message: str):
+        self.echoed_message = echoed_message
+
 
 class StepResult:
-def **init**(self, observation, reward: float, done: bool):
-self.observation = observation
-self.reward = reward
-self.done = done
-
+    def __init__(self, observation, reward: float, done: bool):
+        self.observation = observation
+        self.reward = reward
+        self.done = done
