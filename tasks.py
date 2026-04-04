9d025dc6e3774a622a725130cc23efae3345d00d
Addapu Praharika <praharika18@users.noreply.huggingface.co>
2026-04-04 16:51:59 +0000

    Delete tasks.py

--- tasks.py
+++
@@ -1,7 +0,0 @@
-tasks = [
-    {"job": "cotton harvesting", "required_skill": "cotton", "difficulty": "easy"},
-    {"job": "pesticide spraying", "required_skill": "spraying", "difficulty": "hard"},
-    {"job": "rice planting", "required_skill": "rice", "difficulty": "medium"},
-    {"job": "tractor ploughing", "required_skill": "tractor", "difficulty": "hard"},
-    {"job": "irrigation management", "required_skill": "water", "difficulty": "medium"}
-]
