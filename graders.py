d9999d66dfcc72689cef4788763acef5a6eb589a
AddapuPraharika <praharikaaddapu@gmail.com>
2026-04-08 14:18:50 +0530

    Refactor grade functions to include default difficulty

--- graders.py
+++ graders.py
@@ -6,20 +6,20 @@ def grade_easy(farmer_skill, required_skill):
     else:
         return 0.05
 
-def grade_medium(farmer_skill, required_skill, difficulty):
+
+def grade_medium(farmer_skill, required_skill, difficulty=None):
     if farmer_skill == required_skill:
         return 0.9
-    elif difficulty == "medium" and farmer_skill in ["cotton", "rice", "spraying"]:
+    elif farmer_skill in ["cotton", "rice", "spraying"]:
         return 0.45
     else:
         return 0.1
 
-def grade_hard(farmer_skill, required_skill, difficulty):
-    if farmer_skill == required_skill and difficulty == "hard":
+
+def grade_hard(farmer_skill, required_skill, difficulty=None):
+    if farmer_skill == required_skill:
         return 0.85
-    elif farmer_skill == required_skill:
-        return 0.7
-    elif difficulty == "hard":
-        return 0.15
+    elif farmer_skill in ["cotton", "rice", "spraying", "tractor"]:
+        return 0.2
     else:
         return 0.1
