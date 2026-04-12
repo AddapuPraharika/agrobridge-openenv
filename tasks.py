5a1d0f412a596d9cd4cce3d17336b3f24ac46c7f
AddapuPraharika <praharikaaddapu@gmail.com>
2026-04-12 21:43:52 +0530

    Refactor tasks list and remove unused graders

--- tasks.py
+++ tasks.py
@@ -1,39 +1,27 @@
-from graders import grade_easy, grade_medium, grade_hard
-
 tasks = [
     {
-        "name": "easy",
         "job": "cotton harvesting",
         "required_skill": "cotton",
-        "difficulty": "easy",
-        "grader": grade_easy,
+        "difficulty": "easy"
     },
     {
-        "name": "medium",
-        "job": "rice planting",
-        "required_skill": "rice",
-        "difficulty": "medium",
-        "grader": grade_medium,
-    },
-    {
-        "name": "hard",
         "job": "pesticide spraying",
         "required_skill": "spraying",
-        "difficulty": "hard",
-        "grader": grade_hard,
+        "difficulty": "hard"
     },
     {
-        "name": "medium",
-        "job": "irrigation management",
-        "required_skill": "water",
-        "difficulty": "medium",
-        "grader": grade_medium,
+        "job": "rice planting",
+        "required_skill": "rice",
+        "difficulty": "medium"
     },
     {
-        "name": "hard",
         "job": "tractor ploughing",
         "required_skill": "tractor",
-        "difficulty": "hard",
-        "grader": grade_hard,
+        "difficulty": "hard"
     },
+    {
+        "job": "irrigation management",
+        "required_skill": "water",
+        "difficulty": "medium"
+    }
 ]
